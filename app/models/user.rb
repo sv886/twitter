cf3f6f553313dfe949cedc564d8322d1ee229587
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2016-03-13 21:46:43 -0500

    upload profile image at registration, remove photo validation from user for login test

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   acts_as_followable
   acts_as_follower
 
-  validates :username, :password, :photo, presence: true
+  validates :username, :password, presence: true
   attachment :photo
 
 end
